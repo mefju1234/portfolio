fd638d8c1f17680f1eeb0ead8e815a3e55fc5ab9
mefju1234 <mwolek350@gmail.com>
2023-09-07 08:52:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <div class="first">
+        <h1>O mnie</h1>
+    </div>
+    <div class="second">
+        <h1>Moje projekty</h1>
+    </div>
+    <div class="third">Kontakt</div>
+    <div class="fourth">Moje umiejętności</div>
 </body>
 </html>
